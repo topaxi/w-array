e390c12ad971c90d848804f26553ed611583c42a
Damian Senn <damian.senn@gmail.com>
2019-05-30 20:01:43 +0200

    Add test with newlines

--- test.js
+++ test.js
@@ -4,14 +4,22 @@ const w = require('.')
 describe('w', () => {
   describe('with simple string', () => {
     it('should create a word array', () => {
-      expect(w`foo bar baz`).to.deep.equal([ 'foo', 'bar', 'baz' ])
+      expect(w`foo bar baz`).to.deep.equal(['foo', 'bar', 'baz'])
+    })
+
+    it('should work with newlines', () => {
+      expect(w`
+        foo
+        bar
+        baz
+      `).to.deep.equal(['foo', 'bar', 'baz'])
     })
   })
 
   describe('with interpolation', () => {
     it('should interpolate values', () => {
-      expect(w`foo ${'bar'} baz`).to.deep.equal([ 'foo', 'bar', 'baz' ])
-      expect(w`foo${'bar'}baz`).to.deep.equal([ 'foobarbaz' ])
+      expect(w`foo ${'bar'} baz`).to.deep.equal(['foo', 'bar', 'baz'])
+      expect(w`foo${'bar'}baz`).to.deep.equal(['foobarbaz'])
     })
   })
 })
